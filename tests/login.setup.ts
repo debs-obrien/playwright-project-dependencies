88bc72139896bb0a1f039922f64d68735864bc1e
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2023-03-14 13:35:07 +0100

    test: break test to see traces

--- tests/login.setup.ts
+++ tests/login.setup.ts
@@ -4,7 +4,7 @@ import { STORAGE_STATE } from '../playwright.config';
 setup('do login', async ({ page }) => {
   await page.goto('/');
   await page.getByRole('link', { name: 'Log in' }).click();
-  await page.getByPlaceholder('Enter your username').fill(process.env.USERNAME!);
+  await page.getByPlaceholder('Enter your username').fill(process.env.PASSWORD!);
   await page.getByPlaceholder('Enter your password').fill(process.env.PASSWORD!);
   await page.getByRole('button', { name: 'Log in' }).click();
 
