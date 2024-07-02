729c41a53b175d01781127a2e5efc103720faf0e
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2024-07-02 13:55:23 +0200

    updates

--- tests/e2e-menu-loggedin.spec.ts
+++ tests/e2e-menu-loggedin.spec.ts
@@ -8,10 +8,8 @@ test('menu', async ({ page }) => {
   await page.getByRole('link', { name: 'TestingLogin' }).click();
   await expect(page.getByRole('heading', { name: /TestingLogin/i })).toBeVisible();
   await page.getByRole('link', { name: /alerts/i  }).click();
-  await expect(page.getByText('Alerts')).toBeVisible();
-  await page.getByRole('button', { name: /notice/i  }).click();
-  await expect(page.getByText('Notices', { exact: true })).toBeVisible();
-  await page.getByRole('link', { name: /watchlist/i  }).click();
+  await expect(page.getByText('Alerts (5)')).toBeVisible();
+  await page.locator('#pt-watchlist-2').getByRole('link', { name: 'Watchlist' }).click();
   await expect(page.getByRole('heading', { name: 'Watchlist' })).toBeVisible();
 
 })
